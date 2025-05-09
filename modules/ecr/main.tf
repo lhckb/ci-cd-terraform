1cd297f61ef95cfc99812534cff3bf716f3ccf8e
Luís Cruz <lcruz3@apple.com>
2025-05-09 09:27:18 -0300

    test pipeline

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "todo_app_repo" {
   name = var.repository_name
-
+  force_delete = true
+  
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
   }
